07c01176468c8c6b3fd9b7da94d450f72d469573
张大伟 <zhdwwf@163.com>
2021-06-17 16:41:31 +0800

    添加项目文件。

---
+++ ExcelExportDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Magicodes.ExporterAndImporter.Core;
+using Magicodes.ExporterAndImporter.Excel;
+
+namespace ExcelExportDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IExporter exporter = new ExcelExporter();
+            var data = new List<ExportTestDataWithPicture>();
+            data.Add(new ExportTestDataWithPicture
+            {
+                Img = "http://image-demo.oss-cn-hangzhou.aliyuncs.com/example.jpg"
+            });
+
+            var filePath = Path.Combine(AppContext.BaseDirectory, "test.xlsx");
+            var result = await exporter.Export(filePath, data);
+            Console.WriteLine(result.FileName);
+        }
+    }
+
+    [ExcelExporter(Name = "测试")]
+    public class ExportTestDataWithPicture
+    {
+        [ExportImageField(Width = 50, Height = 120, Alt = "404")]
+        [ExporterHeader(DisplayName = "图", IsAutoFit = false)]
+        public string Img { get; set; }
+    }
+}
