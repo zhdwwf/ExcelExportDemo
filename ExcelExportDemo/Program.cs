ae1b39f485ddb4c8452953e3bae3b2c033b1d621
张大伟 <zhdwwf@163.com>
2021-06-18 09:02:10 +0800

    update demo

--- ExcelExportDemo/Program.cs
+++ ExcelExportDemo/Program.cs
@@ -9,18 +9,23 @@ namespace ExcelExportDemo
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
+            string format=args[0];
+            string img=format.ToLower() == "png" ? "https://gitee.com/magicodes/Magicodes.IE/raw/master/docs/Magicodes.IE.png" : "https://gitee.com/magicodes/Magicodes.IE/raw/master/res/wechat.jpg";
             IExporter exporter = new ExcelExporter();
-            var data = new List<ExportTestDataWithPicture>();
-            data.Add(new ExportTestDataWithPicture
-            {
-                Img = "http://image-demo.oss-cn-hangzhou.aliyuncs.com/example.jpg"
-            });
 
-            var filePath = Path.Combine(AppContext.BaseDirectory, "test.xlsx");
-            var result = await exporter.Export(filePath, data);
-            Console.WriteLine(result.FileName);
+            var data = new List<ExportTestDataWithPicture>
+            {
+                new ExportTestDataWithPicture
+                {
+                    Img =img
+                }
+            };
+
+            var filePath = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "test.xlsx");
+            var result = exporter.Export("test.xlsx", data).Result;
+            Console.WriteLine("导出成功！");
         }
     }
 
